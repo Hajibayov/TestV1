615897a3fa3c4bc04cdb29b3883880a1db00e971
Hajibayov <shacibyov@gmail.com>
2023-12-07 10:41:24 +0400

    Saved on Thu 12/07/2023-10:41:24.45

--- commit.bat
+++ commit.bat
@@ -1,3 +1,14 @@
+@ECHO OFF
+
+SET comment=Saved on %date%-%time%
+
+IF "%~1"=="" GOTO COMMIT
+SET username=%1
+SET comment=%comment% by %username%
+
+:COMMIT
+ECHO %comment%
+git checkout development
 git add .
-git commit -m "some changes"
-git push
+git commit -m "%comment%"
+git push origin
